fd7d863de65d475078e968778516c7b8888c16be
EmilioTrujillo03 <ejtrujillo@puce.edu.ec>
2023-10-20 18:56:06 -0500

    update

---
+++ Scripts/Pseudocode.R
@@ -0,0 +1,11 @@
+# Escribe un código que imprima un número y su cuadrado a lo largo de un rango de valores
+# También que imprima la suma de todos los cuadrados de dicho rango
+
+# Define el valor mínimo y el valor máximo
+
+# Crea una variable squaresum que tenga al inicio 0
+
+# Loop a lo largo del rango y por cada valor:
+	# Imprime el valor y el valor al cuadrado
+	# Adiciona el valor al cuadrado a la variable que llamé squaresum
+# Una vez terminado el loop, imprime la variable squaresum 
---
+++ Scripts/PseudocodeProyecto.R
@@ -0,0 +1,16 @@
+#Para comparar las secuencias de nucleótidos de la región rbcL entre la especie Nototriche phyllanthos y Malva sp.
+# Comparar los nucleótidos que existen en zonas específicas de las secuencias de todas las líneas
+#Hacer un for loop que me imprima las zonas específicas de ambas secuencias-
+
+# Cambiar la secuencia .txt de Nototriche a .fasta, al igual que la de Malvastrum
+
+# Eliminar los espacios existentes en el encabezado del archivo de las secuencias
+
+# Cargar las secuencias a objetos en RStudio
+
+# Crear un for loop que me imprima los datos requeridos
+	# Que imprima cada línea de ambas secuencias, una abajo de la otra.
+	# Que imprima zonas específicas de cada línea, una abajo de la otra.
+	# Que cree nuevos objetos con las zonas comparadas
+
+
