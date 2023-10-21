4918000b5eacab775cf94b36695516fb9d2e2caf
EmilioTrujillo03 <ejtrujillo@puce.edu.ec>
2023-10-21 09:17:05 -0500

    New

--- Scripts/Pseudocode.R
+++ Scripts/Pseudocode.R
@@ -2,10 +2,27 @@
 # También que imprima la suma de todos los cuadrados de dicho rango
 
 # Define el valor mínimo y el valor máximo
+lower = 1
+Upper = 5
 
 # Crea una variable squaresum que tenga al inicio 0
+
+Squaresum = 0
 
 # Loop a lo largo del rango y por cada valor:
 	# Imprime el valor y el valor al cuadrado
 	# Adiciona el valor al cuadrado a la variable que llamé squaresum
+
+for (i in lower:Upper) {
+  cat (i, i^2, "\n")
+  Squaresum = Squaresum + i^2
+}
+
+cat("The sum of it all is", Squaresum)
 # Una vez terminado el loop, imprime la variable squaresum 
+
+
+
+
+
+
